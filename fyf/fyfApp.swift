85004295dae83e9b9811e54288ed49446613c232
Brian Majewski <brian.majewski@gmail.com>
2023-10-10 14:00:46 -0700

    Initial Commit

---
+++ fyf/fyfApp.swift
@@ -0,0 +1,17 @@
+//
+//  fyfApp.swift
+//  fyf
+//
+//  Created by Brian Majewski on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct fyfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fyfUITests/fyfUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  fyfUITestsLaunchTests.swift
+//  fyfUITests
+//
+//  Created by Brian Majewski on 10/10/23.
+//
+
+import XCTest
+
+final class fyfUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
